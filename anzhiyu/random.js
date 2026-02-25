c5bfe66a438fe557b3e186921d7d7d42a0b68910
Austoin <liuzhigui_2023@qq.com>
2026-02-25 22:08:14 +0800

    Site updated: 2026-02-25 22:08:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/hexo-blog-guide/","2025/11/04/vpn-record/","2026/01/21/免费域名注册与Cloudflare绑定教程/","2026/01/15/vue-deployment-guide/"];function toRandomPost(){
+var posts=["2025/11/04/3-vpn-record/","2025/06/08/1-hexo-blog-guide/","2026/01/21/2-免费域名注册与Cloudflare绑定教程/","2026/01/15/4-vue-deployment-guide/","2026/02/24/5-leetcode-2026-2-24/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
