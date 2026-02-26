6523521182642aa4446dcdb42ac376ffc0e76431
Austoin <liuzhigui_2023@qq.com>
2026-02-26 18:41:55 +0800

    Site updated: 2026-02-26 18:41:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/1-hexo-blog-guide/","2026/01/21/2-免费域名注册与Cloudflare绑定教程/","2025/11/04/3-vpn-record/","2026/01/15/4-vue-deployment-guide/","2026/02/25/6-leetcode-2026-2-25/","2026/02/24/5-leetcode-2026-2-24/","2026/02/26/7-快速过一遍golang核心/"];function toRandomPost(){
+var posts=["2026/01/21/2-免费域名注册与Cloudflare绑定教程/","2025/06/08/1-hexo-blog-guide/","2025/11/04/3-vpn-record/","2026/02/24/5-leetcode-2026-2-24/","2026/02/25/6-leetcode-2026-2-25/","2026/01/15/4-vue-deployment-guide/","2026/02/26/7-快速过一遍golang核心/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
